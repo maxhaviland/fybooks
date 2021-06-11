683d0e814ee656c2fb706fbc52d79da76ce47503
maxhaviland <maxhaviland@gmail.com>
2021-06-11 20:50:37 -0300

    fix errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,10 @@
 import { EventHandler, FormEvent, useContext, useRef } from 'react';
-import { Input, Search } from '../components';
-import useSearch from '../hooks/useSearch';
+import { Search } from '../components';
 
 function Index() {
-  const searchInputRef = useRef<HTMLInputElement>(null);
-  const { execute, error } = useSearch()
-
-  const submit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    execute(searchInputRef.current.value)
-  }
-
   return (
     <>
-      <Search onSubmit={submit}>
-        <Input ref={searchInputRef} />
-        {error}
-      </Search>
+      <Search />
       fybooks :(
     </>
   )
